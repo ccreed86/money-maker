ec19c5993d0d703ae35d40d27cb5bcd3bc33af85
Clifton <cliftonreed1986@gmail.com>
2024-05-02 19:19:17 -0700

    models completed

--- models/index.js
+++ models/index.js
@@ -1,23 +1,28 @@
-// √ import models
+// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 Product.hasOne(Category, {
-  foreignKey: 'driver_id',
+  foreignKey: 'category_id',
   // When we delete a Driver, make sure to also delete the associated License.
   onDelete: 'CASCADE',
 });
 
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete: 'CASCADE', 
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: 'ProductTag' })
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'ProductTag' })
 
 module.exports = {
   Product,
